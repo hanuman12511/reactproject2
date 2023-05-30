36a901729a3068b9112f370ab92fc29cfefd1959
hanumansaini12511 <er.hanumansaini@gmail.com>
2023-05-30 15:11:52 +0530

    fix

---
+++ src/view/screen/Home.js
@@ -0,0 +1,11 @@
+import Product from "./Product";
+
+export default function Home(){
+    return(
+        <>
+        <div style={{width:'100%' ,height:200,backgroundColor:'red'}}>
+        <h1>Home screen</h1>
+      </div>
+        </>
+    )
+}
---
+++ src/view/screen/Product.js
@@ -0,0 +1,10 @@
+import Details from "./Details";
+
+export default function Product(){
+    return(
+        <>
+        <h1>Product screen</h1>
+       
+        </>
+    )
+}
